e08b8e1ae3a8d80aff9ac5b106d0949e475a0733
khalil-kitar <118304270+khalil-kitar@users.noreply.github.com>
2024-05-09 19:13:59 +0200

    first commit

---
+++ Assets/Scripts/Bullet1.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet1 : MonoBehaviour
+{
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Vérifie si l'objet de la collision a le tag "Target"
+        if (collision.gameObject.CompareTag("Target"))
+        {
+            print("Hit"+ collision.gameObject.name +"Target!");
+            //Destroy(gameObject);  // Détruit l'objet auquel ce script est attaché
+        }
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float bulletVelocity=30;
+    public float bulletPrefabTime=3f;
+
+    public Weapon()
+    {
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            FireWeapon();
+        }
+        
+    }
+
+    private void FireWeapon()
+    {
+        GameObject bullet = Instantiate(bulletPrefab,bulletSpawn.position, Quaternion.identity);
+
+        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward.normalized*bulletVelocity, ForceMode.Impulse);
+    
+        StartCoroutine(DestroyBulletAfterTime(bullet, bulletPrefabTime));
+    }
+
+    private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(bullet);
+    }
+
+}
