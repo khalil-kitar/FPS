3548911d9e77c69cd884ecb6befa89a48f46962d
cherkaouianas <131997959+cherkaouianas@users.noreply.github.com>
2024-05-10 22:58:46 +0100

    commit

---
+++ Assets/Scripts/MouseMouvement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMouvement : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+    float topClamp = -90f;
+    float bottomClamp = 90f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation,topClamp,bottomClamp);
+
+        yRotation += mouseX;
+
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation,0f);
+
+
+
+    }
+}
